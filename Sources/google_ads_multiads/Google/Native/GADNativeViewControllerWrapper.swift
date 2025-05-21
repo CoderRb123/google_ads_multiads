c54eb83110fa08e196d427748bc5263d7e64b6a8
Bukhari Rehan Abbas <44895678+CoderRb123@users.noreply.github.com>
2025-05-21 15:45:51 +0530

    xxxsas

--- Sources/google_ads_multiads/Google/Native/GADNativeViewControllerWrapper.swift
+++ Sources/google_ads_multiads/Google/Native/GADNativeViewControllerWrapper.swift
@@ -39,58 +39,64 @@ public struct GoogleNativeAd : View{
         self.height = height
         self.width = width
         self.from = from
-        if(from != nil){
-            let server = ServerConfig.sharedInstance.screenConfig?[from!]
-            config = server ?? ServerConfig.sharedInstance.screenConfig?["default"]
-        }else{
-            config =  ServerConfig.sharedInstance.screenConfig?["default"]
-
-        }
+      
     }
 
     @available(iOS 13.0, *)
     public var body: some View{
         let calPadding = min(width * 1.5 / 100, 10)
         
-        if(!ServerConfig.sharedInstance.globalAdStatus){
-            VStack {}
-        }else{
-            if(config!.showAds){
-                if(config!.native == 0){
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 18)
-                            .fill(.white)
-                            .shadow(
-                                   color: Color.black.opacity(0.3),
-                                   radius: 0,                           x: 0,
-                                   y: 5
-                            )
-                        
-                        
-                        Group{
-                            Rectangle()
-                                .strokeBorder(Color(ColorFunctions.hexStringToUIColor(hex: "#ffce64")),style: StrokeStyle(lineWidth: 3, dash: [10]))
-                                       .clipShape(RoundedRectangle(cornerRadius: 15.0))
-                                       .padding(.vertical,paddingDottedLine)
-                                       .padding(.horizontal,calPadding)
-                                      
+       
+        
+        VStack {
+            if(!ServerConfig.sharedInstance.globalAdStatus){
+                VStack {}
+            }else{
+                if(config!.showAds){
+                    if(config!.native == 0){
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 18)
+                                .fill(.white)
+                                .shadow(
+                                       color: Color.black.opacity(0.3),
+                                       radius: 0,                           x: 0,
+                                       y: 5
+                                )
                             
-                            // Yellow Tile
+                            
+                            Group{
+                                Rectangle()
+                                    .strokeBorder(Color(ColorFunctions.hexStringToUIColor(hex: "#ffce64")),style: StrokeStyle(lineWidth: 3, dash: [10]))
+                                           .clipShape(RoundedRectangle(cornerRadius: 15.0))
+                                           .padding(.vertical,paddingDottedLine)
+                                           .padding(.horizontal,calPadding)
+                                          
+                                
+                                // Yellow Tile
 
-                            GADNativeViewControllerWrapper() .padding(.vertical,yellowTilePadding).background(Color(ColorFunctions.hexStringToUIColor(hex: "#ffce64")).opacity(0.3))
-                                .clipShape(RoundedRectangle(cornerRadius: 15.0))
-                                .padding(.vertical,10)
-                                .padding(.horizontal,calPadding)
-                        }
-                       
-                    }.frame(height: 320)
-                        .padding(10)
+                                GADNativeViewControllerWrapper() .padding(.vertical,yellowTilePadding).background(Color(ColorFunctions.hexStringToUIColor(hex: "#ffce64")).opacity(0.3))
+                                    .clipShape(RoundedRectangle(cornerRadius: 15.0))
+                                    .padding(.vertical,10)
+                                    .padding(.horizontal,calPadding)
+                            }
+                           
+                        }.frame(height: 320)
+                            .padding(10)
+                    }else{
+                        VStack {}
+                    }
+                   
                 }else{
                     VStack {}
                 }
-               
+            }
+        }.onAppear {
+            if(from != nil){
+                let server = ServerConfig.sharedInstance.screenConfig?[from!]
+                config = server ?? ServerConfig.sharedInstance.screenConfig?["default"]
             }else{
-                VStack {}
+                config =  ServerConfig.sharedInstance.screenConfig?["default"]
+
             }
         }
         
