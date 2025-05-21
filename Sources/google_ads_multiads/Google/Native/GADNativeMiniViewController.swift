d28e07ca4b39481fc092e1c7186a21384564dbbd
Bukhari Rehan Abbas <44895678+CoderRb123@users.noreply.github.com>
2025-05-22 01:52:26 +0530

    ssss

--- Sources/google_ads_multiads/Google/Native/GADNativeMiniViewController.swift
+++ Sources/google_ads_multiads/Google/Native/GADNativeMiniViewController.swift
@@ -153,11 +153,13 @@ extension GADNativeMiniViewController: @preconcurrency NativeAdLoaderDelegate {
 //    (nativeAdView.bodyView as? UILabel)?.text = nativeAd.body
 //    nativeAdView.bodyView?.isHidden = nativeAd.body == nil
 
+    
+    
     (nativeAdView.callToActionView as? UIButton)?.setTitle(nativeAd.callToAction, for: .normal)
     nativeAdView.callToActionView?.isHidden = nativeAd.callToAction == nil
 
-    (nativeAdView.iconView as? UIImageView)?.image = nativeAd.icon?.image
-    nativeAdView.iconView?.isHidden = nativeAd.icon == nil
+//    (nativeAdView.iconView as? UIImageView)?.image = nativeAd.icon?.image
+//    nativeAdView.iconView?.isHidden = nativeAd.icon == nil
 
 //    (nativeAdView.starRatingView as? UIImageView)?.image = imageOfStars(from: nativeAd.starRating)
 //    nativeAdView.starRatingView?.isHidden = nativeAd.starRating == nil
